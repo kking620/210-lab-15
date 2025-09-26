9bd34aaac50c3c2854122187cec17dfa414095d5
Krispy King <kking620@gmail.com>
2025-09-26 16:49:47 -0700

    Second  commit

---
+++ 210-lab-15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Movie
+{
+    private:
+    string 
+    public:
+};
+
+int main()
+{
+
+}
