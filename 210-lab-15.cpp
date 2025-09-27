193809d279d958abcddfc6e781d8e4fb8c89b40b
Krispy King <kking620@gmail.com>
2025-09-26 17:01:41 -0700

    Third  commit

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -9,11 +9,65 @@ using namespace std;
 class Movie
 {
     private:
-    string 
+    string mTitle;
+    string mScreenWriter;
+    int mRelease;
+
     public:
+    string getmTitle()
+    {
+        return mTitle;
+    }
+    void setmTitle(string t)
+    {
+        mTitle = t;
+    }
+     string getmScreenWriter()
+    {
+        return mScreenWriter;
+    }
+    void setmScreenWriter(string sw)
+    {
+        mScreenWriter = sw;
+    }
+     int getmRelease()
+    {
+        return mRelease;
+    }
+    void setmRelease(int y)
+    {
+        mRelease = y;
+    }
 };
 
 int main()
 {
+    vector<Movie> movies_v;
+
+    ifstream fin ("input.txt");
+
+    string title;
+    string screenWriter;
+    int year;
+    int i = 0;
+
+    if (fin.good())
+    {
+        while ( fin >> title)
+        {
+            fin.ignore();
+            fin >> year;
+            Movie temp;
+            temp.setmTitle(title);
+            temp.setmScreenWriter(screenWriter);
+            temp.setmRelease(year);
+            movies_v.push_back(temp);
+
+            i++;
+        }
+        fin.close();
+    }
+    else
+        cout << "Input file not found.\n";
 
 }
