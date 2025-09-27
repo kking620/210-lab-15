cecd289ff6c8edd033613cd65b1018fbea20ff56
Krispy King <kking620@gmail.com>
2025-09-26 17:18:47 -0700

    Fourth commit

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -38,6 +38,13 @@ class Movie
     {
         mRelease = y;
     }
+
+    void print()
+    {
+        cout << "Movie: " << mTitle << endl;
+        cout << setw(15) << "Year Released: " << mRelease << endl;
+        cout << setw(15) << "Screenwriter: " << mScreenWriter << endl;
+    }
 };
 
 int main()
@@ -49,25 +56,32 @@ int main()
     string title;
     string screenWriter;
     int year;
-    int i = 0;
 
     if (fin.good())
     {
-        while ( fin >> title)
+        while ( fin.good())
         {
+            getline(fin, title);
             fin.ignore();
             fin >> year;
+            getline(fin, screenWriter);
             Movie temp;
             temp.setmTitle(title);
-            temp.setmScreenWriter(screenWriter);
             temp.setmRelease(year);
+            temp.setmScreenWriter(screenWriter);
             movies_v.push_back(temp);
-
-            i++;
         }
         fin.close();
     }
     else
         cout << "Input file not found.\n";
 
+    for (auto val : movies_v)
+    {
+        cout << val.print() << endl;
+    }
+
+    cout << endl;
+
+    return 0;
 }
